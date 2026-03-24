1995c4517a68f9d425f5e98f389e1aa5333a336f
Mathiaszero <55817193+Mathiaszero@users.noreply.github.com>
2026-03-24 03:34:07 +0100

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,70 @@
+minetest.register_node("classicmese:mese1", {
+        description = "Transition Mese",
+        tile_images = {"classicmese_mese2.png"},
+        is_ground_content = true,
+        groups = {cracky=1},
+        sounds = default.node_sound_defaults(),
+})
+
+minetest.register_node("classicmese:mese2", {
+        description = "Classic Mese",
+        tile_images = {"classicmese_mese1.png"},
+        is_ground_content = true,
+        groups = {cracky=1},
+        sounds = default.node_sound_defaults(),
+})
+--generation
+
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "classicmese:mese2",
+	wherein        = "default:stone",
+        clust_scarcity = 12*12*12,
+        clust_num_ores = 3,
+        clust_size = 2,
+	height_min     = -31000,
+	height_max     = -40,
+	flags          = "absheight",
+})
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "classicmese:mese1",
+	wherein        = "default:stone",
+        clust_scarcity = 12*12*12,
+        clust_num_ores = 3,
+        clust_size = 2,
+	height_min     = -31000,
+	height_max     = -40,
+	flags          = "absheight",
+})
+
+--crafting
+minetest.register_craft({
+        output = 'default:pick_mese',
+        recipe = {
+                {'classicmese:mese1', 'classicmese:mese1', 'classicmese:mese1'},
+                {'', 'default:stick', ''},
+                {'', 'default:stick', ''},
+        }
+})
+
+minetest.register_craft({
+        output = 'default:pick_mese',
+        recipe = {
+                {'classicmese:mese2', 'classicmese:mese2', 'classicmese:mese2'},
+                {'', 'default:stick', ''},
+                {'', 'default:stick', ''},
+        }
+})
+--Fuel
+minetest.register_craft({
+        type = "fuel",
+        recipe = "classicmese:mese1",
+        burntime = 30,
+})
+
+minetest.register_craft({
+        type = "fuel",
+        recipe = "classicmese:mese2",
+        burntime = 30,
+})
